312c27bd03824041484f9c44e10cf976dbf80c06
kevin <wongkokwah@gmail.com>
2024-02-28 18:55:45 +0800

    add dependency & setup basic express server

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import chalk from "chalk";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// express middleware to parse incoming req to json payload
+app.use(express.json());
+
+// middleware deals with URL-encoded form data
+// decide between utilizing the query-string library (with `extended: true`)
+// or the built-in `querystring` module (with `extended: false`)
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.get("/api/v1/test", (req, res) => {
+  res.json({ data: "hello World!" });
+});
+
+const PORT = process.env.PORT || 1997;
+
+app.listen(PORT, () => {
+  console.log(
+    `${chalk.green.bold("✅")} 👍 Server running in ${chalk.yellow.bold(process.env.NODE_ENV)} mode on ${chalk.blue.bold(PORT)}`
+  );
+});
